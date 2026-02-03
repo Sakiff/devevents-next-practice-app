e6bfa498a6483a86fede192e30e144543e1ecffd
Sakiff <ftliyevsakif@gmail.com>
2026-02-03 14:41:50 +0400

    failed deployment

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,15 @@ import EventCard from "@/components/EventCard";
 import ExploreBtn from "@/components/ExploreBtn";
 import { IEvent } from "@/database";
 import { cacheLife } from "next/cache";
+import { events } from "@/lib/constants";
 
 const Page = async () => {
   "use cache";
   cacheLife("hours");
 
-  const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
-  const response = await fetch(`${BASE_URL}/api/events`);
-  const { events } = await response.json();
+  // const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+  // const response = await fetch(`${BASE_URL}/api/events`);
+  // const { events } = await response.json();
 
   return (
     <section>
@@ -30,7 +31,7 @@ const Page = async () => {
           <ul className="events">
             {events &&
               events.length > 0 &&
-              events.map((event: IEvent) => (
+              events.map((event) => (
                 <li key={event.title}>
                   <EventCard {...event} />
                 </li>
