994c25d58ef9e87ced451f7b5e90962ca80716b0
Sakiff <ftliyevsakif@gmail.com>
2026-02-03 12:08:50 +0400

    implemented caching

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import EventCard from "@/components/EventCard";
 import ExploreBtn from "@/components/ExploreBtn";
 import { IEvent } from "@/database";
+import { cacheLife } from "next/cache";
 
 const Page = async () => {
+  "use cache";
+  cacheLife("hours");
+
   const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
   const response = await fetch(`${BASE_URL}/api/events`);
   const { events } = await response.json();
